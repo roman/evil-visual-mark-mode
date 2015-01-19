7c18caab1c7f51436f9b4ea52d26556f7ef6bb60
Dewdrops <v_v_4474@126.com>
2015-01-19 13:09:51 +0800

    `defvar' form cannot have named arguments

--- evil-visual-mark-mode.el
+++ evil-visual-mark-mode.el
@@ -28,8 +28,7 @@
 
 ;;; Code:
 (defvar evil-visual-mark-overlay-alist '()
-  "List of evil visual mark overlays."
-  :group 'evil-visual-mark)
+  "List of evil visual mark overlays.")
 
 (defface evil-visual-marker-face
   '((t (:foreground "white"
